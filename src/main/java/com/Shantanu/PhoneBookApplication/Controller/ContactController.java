b4e3c8f3f066a81809140fbe5eda2de409db585e
Shantanu-21 <shantanumogre21@gmail.com>
2022-06-19 16:36:51 +0530

    PHN-1 Sprint 2 Write method declaration into contactService

--- src/main/java/com/Shantanu/PhoneBookApplication/Controller/ContactController.java
+++ src/main/java/com/Shantanu/PhoneBookApplication/Controller/ContactController.java
@@ -1,8 +1,34 @@
 package com.Shantanu.PhoneBookApplication.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import com.Shantanu.PhoneBookApplication.Model.Contact;
+import com.Shantanu.PhoneBookApplication.Service.ContactServiceIMPL;
+
+@Controller
 public class ContactController {
+
+	@Autowired
+	private ContactServiceIMPL contactServiceIMPL;
+
+	@PostMapping(value = "/saveContact", consumes = "APPLICATION/JSON")
+	public ResponseEntity<String> savecontact(@RequestBody Contact contact) {
+
+		boolean savecontact = contactServiceIMPL.savecontact(contact);
+		if (savecontact = true) {
+			String msg = "Contact save successfully";
+			return new ResponseEntity<String>(msg, HttpStatus.OK);
+		} else {
+
+			String msg = "contact not save";
+			return new ResponseEntity<String>(msg, HttpStatus.BAD_REQUEST);
+		}
+	}
 
 }
--- src/main/java/com/Shantanu/PhoneBookApplication/Service/ContactServiceIMPL.java
+++ src/main/java/com/Shantanu/PhoneBookApplication/Service/ContactServiceIMPL.java
@@ -1,8 +1,22 @@
 package com.Shantanu.PhoneBookApplication.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
-@Service
-public class ContactServiceIMPL {
+import com.Shantanu.PhoneBookApplication.Model.Contact;
+import com.Shantanu.PhoneBookApplication.Repository.ContactRepository;
 
+@Service
+public class ContactServiceIMPL implements ContactServiceI {
+
+	@Autowired
+	private ContactRepository contactRepository;
+
+	@Override
+	public boolean savecontact(Contact contact) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
 }
