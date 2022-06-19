6fd58bcceea22c070dbcda0831c6febbe5228e73
Shantanu-21 <shantanumogre21@gmail.com>
2022-06-19 15:55:45 +0530

    PHN Sprint 2 PHN-1 Write method declaration into contactService

--- src/main/java/com/Shantanu/PhoneBookApplication/Service/ContactServiceI.java
+++ src/main/java/com/Shantanu/PhoneBookApplication/Service/ContactServiceI.java
@@ -1,5 +1,9 @@
 package com.Shantanu.PhoneBookApplication.Service;
 
+import com.Shantanu.PhoneBookApplication.Model.Contact;
+
 public interface ContactServiceI {
+	
+	boolean savecontact (Contact contact);
 
 }
